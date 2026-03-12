a983bf03f18ba2f21e80dd1508b90e295522e08e
AfeefAhmer <s2401102@edu.bc.fi>
2026-03-12 09:19:58 +0200

    SQLite tehtävät 1-4

---
+++ My project/Assets/Scripts/Login.cs
@@ -0,0 +1,45 @@
+﻿using SQLite;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Pelaajan pinkoodi ja lempinimi
+public class PlayerLogin
+{
+    public string pincode { get; set; }
+    public string nickname { get; set; }
+}
+
+// Kirjautuminen
+public class Login : MonoBehaviour
+{
+    private string database = $"{Application.dataPath}/AdventureGame.db";
+
+    [SerializeField] private TMP_InputField pincode;
+
+    public bool IsLoginOK { get; private set; }
+
+    public void HandleLogin()
+    {
+        var db = new SQLiteConnection(database);
+
+        string sql = $"select * from Login where pincode = '{pincode.text}'";
+
+        var players = db.Query<PlayerLogin>(sql);
+
+        if (players.Count != 0)
+        {
+            print("Pelaaja löytyi");
+            IsLoginOK = true;
+        }
+
+        db.Close();
+
+        if (!IsLoginOK)
+        {
+            return;
+        }
+
+        SceneManager.LoadScene("AdventureGame");
+    }
+}
