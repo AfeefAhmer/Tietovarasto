e4bc61ef902c78ab7d533e2c516854a367bda287
AfeefAhmer <s2401102@edu.bc.fi>
2026-04-10 12:49:42 +0300

    tekstitiedostot Harjoitus 2. Keskustelu NPC-hahmon kanssa

--- Tietovarasto 2/Assets/Adventure Game/Scripts/NPC.cs
+++ Tietovarasto 2/Assets/Adventure Game/Scripts/NPC.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class Npc : MonoBehaviour
 {
+    private void Start()
+    {
+        LogManager.Instance.LogEvent("NPC ilmestyi peliin: " + gameObject.name);
+    }
+
     public void StartDialogue()
     {
         LogManager.Instance.LogEvent("NPC aloitti keskustelun: " + gameObject.name);
